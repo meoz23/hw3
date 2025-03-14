7e7fdd0b50aa487a5d2bf90f0cb7b17566e02e4b
meoz23 <laboumyz@gmail.com>
2025-03-13 21:15:58 -0400

    Implement card flipping animation

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'game_provider.dart';
+import 'card_model.dart';
 
 class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final gameProvider = Provider.of<GameProvider>(context);
+
     return Scaffold(
       appBar: AppBar(title: Text('Card Matching Game')),
       body: Padding(
@@ -13,11 +18,24 @@ class GameScreen extends StatelessWidget {
             crossAxisSpacing: 8,
             mainAxisSpacing: 8,
           ),
-          itemCount: 16,
+          itemCount: gameProvider.cards.length,
           itemBuilder: (context, index) {
-            return Card(
-              color: Colors.blue,
-              child: Center(child: Text('?')),
+            return GestureDetector(
+              onTap: () => gameProvider.flipCard(index),
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                decoration: BoxDecoration(
+                  color: gameProvider.cards[index].isFaceUp ? Colors.white : Colors.blue,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 4)],
+                ),
+                child: Center(
+                  child: Text(
+                    gameProvider.cards[index].isFaceUp ? gameProvider.cards[index].value : '?',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             );
           },
         ),
