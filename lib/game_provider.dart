24ad6a082ffc113667ebba5f9a05fcde031ce8e1
meoz23 <laboumyz@gmail.com>
2025-03-13 21:19:18 -0400

    Implemented game logic

--- lib/game_provider.dart
+++ lib/game_provider.dart
@@ -3,6 +3,8 @@ import 'card_model.dart';
 
 class GameProvider with ChangeNotifier {
   List<CardModel> cards = [];
+  List<int> flippedCards = [];
+  int score = 0;
 
   GameProvider() {
     _initializeCards();
@@ -13,13 +15,33 @@ class GameProvider with ChangeNotifier {
     values.shuffle();
 
     cards = values.map((val) => CardModel(value: val)).toList();
+    flippedCards.clear();
+    score = 0;
     notifyListeners();
   }
 
   void flipCard(int index) {
     if (cards[index].isMatched || cards[index].isFaceUp) return;
 
-    cards[index].isFaceUp = !cards[index].isFaceUp;
-    notifyListeners();
+    cards[index].isFaceUp = true;
+    flippedCards.add(index);
+
+    if (flippedCards.length == 2) {
+      Future.delayed(Duration(seconds: 1), () {
+        if (cards[flippedCards[0]].value == cards[flippedCards[1]].value) {
+          cards[flippedCards[0]].isMatched = true;
+          cards[flippedCards[1]].isMatched = true;
+          score += 10;
+        } else {
+          cards[flippedCards[0]].isFaceUp = false;
+          cards[flippedCards[1]].isFaceUp = false;
+          score -= 2;
+        }
+        flippedCards.clear();
+        notifyListeners();
+      });
+    } else {
+      notifyListeners();
+    }
   }
 }
